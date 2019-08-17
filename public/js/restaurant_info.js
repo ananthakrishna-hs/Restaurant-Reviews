549383ef350f2d93332d1c19a0988b9d1f441b2d
ananthakrishna-hs <h.s.ananthakrishna@gmail.com>
2019-08-17 10:47:21 +0530

    routine

--- public/js/restaurant_info.js
+++ public/js/restaurant_info.js
@@ -22,7 +22,7 @@ var initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: 'pk.eyJ1Ijoic3J0MTAwIiwiYSI6ImNqbXFseXphaDFjMzgzcHFjZ2t0czI1YjQifQ.A-uhHN9qH3t-vrDNgqfCYg',
+        mapboxToken: '<your MAPBOX API KEY HERE>',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
